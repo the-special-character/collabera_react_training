f031dd13739e3789058665af8a9eec5d74c0858b
Yagnesh Modh <yagnesh.modh@gmail.com>
2023-02-07 12:28:51 +0530

    added products

--- src/layouts/mainLayout.jsx
+++ src/layouts/mainLayout.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Navigate, Outlet } from 'react-router-dom';
 import { useAuthContext } from '../context/authContext';
+import Header from '../components/Header';
 
 function MainLayout() {
   const { user } = useAuthContext();
@@ -9,7 +10,12 @@ function MainLayout() {
     return <Navigate to="/auth" replace />;
   }
 
-  return <Outlet />;
+  return (
+    <>
+      <Header />
+      <Outlet />
+    </>
+  );
 }
 
 export default MainLayout;
