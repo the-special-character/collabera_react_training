55a005e5c566ecb9b34f79a5f70ff158a055faba
Yagnesh Modh <yagnesh.modh@gmail.com>
2023-02-13 10:10:50 +0530

    completed middleware

--- src/middlewares/errorMiddleware.js
+++ src/middlewares/errorMiddleware.js
@@ -1,20 +1,41 @@
-import axiosInstance from '../utils/axiosInstance';
-
-const error = store => next => async action => {
-  const {
-    user: { user },
-  } = store.getState();
-  const match = /(.*)_(FAIL)/.exec(action.type);
-
+const error = () => next => async action => {
+  const { type, payload, meta } = action;
+  const match = /(.*)_(FAIL)/.exec(type);
   if (match) {
-    await axiosInstance.post('errors', {
-      user,
-      ...action.payload,
-      createdAt: new Date(),
+    next({
+      type,
+      meta,
+      payload: {
+        message: payload.message,
+        title: `${type
+          .split('_')
+          .map((x, i) => {
+            if (i === 0) {
+              return `${x[0].toUpperCase()}${x.slice(1).toLocaleLowerCase()}`;
+            }
+            return x.toLocaleLowerCase();
+          })
+          .join(' ')} fail`,
+      },
     });
+  } else {
+    next(action);
   }
 
-  next(action);
+  //   const {
+  //     user: { user },
+  //   } = store.getState();
+  //   const match = /(.*)_(FAIL)/.exec(action.type);
+
+  //   if (match) {
+  //     await axiosInstance.post('errors', {
+  //       user,
+  //       ...action.payload,
+  //       createdAt: new Date(),
+  //     });
+  //   }
+
+  //   next(action);
 };
 
 export default error;
--- src/utils/APIRequest.js
+++ src/utils/APIRequest.js
@@ -15,19 +15,21 @@ export default async ({ dispatch, apiData, type, meta }) => {
   } catch (err) {
     dispatch({
       type: `${type}_FAIL`,
-      payload: {
-        message: err.message,
-        title: `${type
-          .split('_')
-          .map((x, i) => {
-            if (i === 0) {
-              return `${x[0].toUpperCase()}${x.slice(1).toLocaleLowerCase()}`;
-            }
-            return x.toLocaleLowerCase();
-          })
-          .join(' ')} fail`,
-      },
+      payload: err,
       meta,
     });
+
+    // payload: {
+    //   message: err.message,
+    //   title: `${type
+    //     .split('_')
+    //     .map((x, i) => {
+    //       if (i === 0) {
+    //         return `${x[0].toUpperCase()}${x.slice(1).toLocaleLowerCase()}`;
+    //       }
+    //       return x.toLocaleLowerCase();
+    //     })
+    //     .join(' ')} fail`,
+    // },
   }
 };
